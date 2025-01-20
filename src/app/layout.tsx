578303438cddae71edbf9e3c3746db3f6f0818b9
Gabriel Killy <gabykilly@gmail.com>
2025-01-20 17:51:02 -0600

    setting up colors, fonts, and folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { IBM_Plex_Serif, Work_Sans, Familjen_Grotesk, Yarndings_12 } from "next/font/google";
+import "../styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ibmPlexSerif = IBM_Plex_Serif({
+  variable: "--font-ibm-plex-serif",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const workSans = Work_Sans({
+  variable: "--font-work-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const familjenGrotesk = Familjen_Grotesk({
+  variable: "--font-familjen-grotesk",
   subsets: ["latin"],
 });
+
+const yarndings12 = Yarndings_12({
+  variable: "--font-familjen-grotesk",
+  weight: "400"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ibmPlexSerif.variable} ${workSans.variable} ${yarndings12.variable} ${familjenGrotesk.variable} antialiased`}
       >
         {children}
       </body>
