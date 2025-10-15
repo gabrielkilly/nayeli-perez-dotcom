b8d26ff71129cec795ea1d81e4591bb3bde6f380
gabrielkilly <gabykilly@gmail.com>
2025-10-15 02:38:16 +0000

    factory os complete

--- src/app/project/[projectId]/content/FactoryOs.tsx
+++ src/app/project/[projectId]/content/FactoryOs.tsx
@@ -101,6 +101,170 @@ export const FactoryOS: ProjectContent = {
                     }
                 }
             ],
+        },
+        {
+            backgroundColorCssName: "bg-[#5F2446]",
+            textColorCssName: "text-type-alt",
+            contentItems: [
+                {
+                    type: "numberedHeader",
+                    content: {
+                        number: 2,
+                        numberClassName: "text-[#C594B8]",
+                        header: "Automated Cut Sheet Generator"
+                    }
+                },
+                {
+                    type: "spacer",
+                    content: {
+                        paddingClassName: "py-0"
+                    }
+                },
+                {
+                    type: "headerAndDescription",
+                    content: {
+                        header: "Timeframe",
+                        description: "September 2024"
+                    }
+                },
+                {
+                    type: "headerAndDescription",
+                    content: {
+                        header: "Objective",
+                        description: "In 2024, Gantri expanded its focus to trade and wholesale revenue streams. One of the needs surfaced during this time was providing potential partners with full product details quickly and easily. I designed product cut sheets and a cut sheet generator functionality that can be accessed as needed by our internal team."
+                    }
+                },
+                {
+                    type: "spacer",
+                    content: {
+                        paddingClassName: "py-2"
+                    }
+                },
+                {
+                    type: "itemGrid",
+                    content: {
+                        items: [
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#8A3867]",
+                                    src: "/projects/factory-os/selected-work-gantri-cutsheet-1.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#8A3867]",
+                                    src: "/projects/factory-os/selected-work-gantri-cutsheet-2.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#8A3867]",
+                                    src: "/projects/factory-os/selected-work-gantri-cutsheet-3.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#8A3867]",
+                                    src: "/projects/factory-os/selected-work-gantri-cutsheet-4.png"
+                                }
+                            }
+                        ]
+                    }
+                }
+            ],
+        },
+        {
+            backgroundColorCssName: "bg-[#333330]",
+            textColorCssName: "text-type-alt",
+            contentItems: [
+                {
+                    type: "numberedHeader",
+                    content: {
+                        number: 3,
+                        numberClassName: "text-[#8A8A85]",
+                        header: "Bulk Asset Uploader"
+                    }
+                },
+                {
+                    type: "spacer",
+                    content: {
+                        paddingClassName: "py-0"
+                    }
+                },
+                {
+                    type: "headerAndDescription",
+                    content: {
+                        header: "Timeframe",
+                        description: "July 2024"
+                    }
+                },
+                {
+                    type: "headerAndDescription",
+                    content: {
+                        header: "Objective",
+                        description: "In 2024, Gantri began offering every product in our catalog in over color. Previously each product was offered in 1-4 color options. This was promising as it allows us to provide the internal team a much better way to update, manage, and maintain product assets.\n\nI designed a bulk asset uploading tool, working closely with the Creative team to understand their workflows and key needs."
+                    }
+                },
+                {
+                    type: "spacer",
+                    content: {
+                        paddingClassName: "py-2"
+                    }
+                },
+                {
+                    type: "itemGrid",
+                    content: {
+                        items: [
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#52534F]",
+                                    src: "/projects/factory-os/selected-work-gantri-assetmanager-1.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#52534F]",
+                                    src: "/projects/factory-os/selected-work-gantri-assetmanager-2.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#52534F]",
+                                    src: "/projects/factory-os/selected-work-gantri-assetmanager-3.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#52534F]",
+                                    src: "/projects/factory-os/selected-work-gantri-assetmanager-4.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#52534F]",
+                                    src: "/projects/factory-os/selected-work-gantri-assetmanager-5.png"
+                                }
+                            },
+                            {
+                                type: "imageCard",
+                                content: {
+                                    bgClassName: "bg-[#52534F]",
+                                    src: "/projects/factory-os/selected-work-gantri-assetmanager-6.png"
+                                }
+                            }
+                        ]
+                    }
+                }
+            ],
         }
     ]
 }
