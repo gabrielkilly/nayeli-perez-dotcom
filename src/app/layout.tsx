774a860e676fa84fc5760cf033db0fdcac1fab7f
gabrielkilly <gabykilly@gmail.com>
2025-10-15 05:03:26 +0000

    better scipt for contentsquare

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import * as React from "react"
+import Script from "next/script";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { fontFamiljenGrotesk, fontLora, fontWorkSans, fontYarndings12 } from "@/components/Fonts";
 import ClientLayout from "@/components/ClientLayout";
@@ -11,12 +12,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script src="https://t.contentsquare.net/uxa/79d198fbdbd04.js" />
-      </head>
+      <head />
       <body
         className={`${fontLora.variable} ${fontWorkSans.variable} ${fontYarndings12.variable} ${fontFamiljenGrotesk.variable} antialiased`}
       >
+        <Script
+          src="https://t.contentsquare.net/uxa/79d198fbdbd04.js"
+          strategy="afterInteractive"
+        />
         <ThemeProvider
         attribute="class"
         defaultTheme="system"
