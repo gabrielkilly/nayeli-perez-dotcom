af91aed15ac694e82f907c916c7220d87383d0cb
gabrielkilly <gabykilly@gmail.com>
2025-10-22 01:35:22 +0000

    inspiration page created

---
+++ src/app/inspiration/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/Footer";
+import { inspirationContent } from "./content"
+
+export default function Inspiration() {
+    const content = inspirationContent;
+
+    return (
+        <>
+            <main>
+                <div className="flex flex-col items-center justify-center">
+                    
+                </div>
+                <Footer />
+            </main>
+        </>
+    )
+}
